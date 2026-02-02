8b654a2b05cb354e368a361cebe95ed22fff3979
David Wilson <davidraywilson@live.com>
2026-02-02 16:17:46 -0600

    Version Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.calmapps.calmmusic"
         minSdk = 28
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.7"
+        versionCode = 10
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
